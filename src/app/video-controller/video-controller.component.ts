6ebe3aba5467e1c0e74af066c8086247be51aed4
Chrys Rakotonimanana <chrysrakk@gmail.com>
2020-07-04 21:11:44 +0300

    adding some functionality to controls video

--- src/app/video-controller/video-controller.component.ts
+++ src/app/video-controller/video-controller.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-video-controller',
@@ -8,9 +8,57 @@ import {Component, Input, OnInit, ViewChild} from '@angular/core';
 export class VideoControllerComponent implements OnInit {
   @Input() public currentTime: number;
   @Input() public duration: number = 0;
-  constructor() { }
+  @Input() public volume: number = 1;
+  @Output() private onSeek: EventEmitter<any> = new EventEmitter<any>();
+  @Output() private onVolumeChange: EventEmitter<any> = new EventEmitter<any>();
+  @ViewChild('seekbar') public seekbar: ElementRef;
+
+  public icons: Record<string, string> = {
+    play: 'play_arrow',
+    pause: 'pause',
+    stop: 'stop',
+    next: 'skip_next',
+    prev: 'skip_previous',
+    repeatOnce: 'repeat_once',
+    repeatAll: 'repeat_all',
+    shuffle: 'shuffle'
+  };
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  seekTo(e) {
+    this.onSeek.emit(e);
+  }
+
+  setVolumeTo(e) {
+    this.onVolumeChange.emit(e);
+  }
+
+  formatLabel(value: number) {
+    return Math.round(value * 100) + '%';
+  }
+
+  updatePlayingState() {
+
+  }
+
+  next() {
+
+  }
+
+  prev() {
+
+  }
+
+  stop() {
+
+  }
+
+  togglePlay() {
+
+  }
 }
